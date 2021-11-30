3e495ce8524a3753cc989ae421ac79df7b4b507a
CYMyZero <50820670+CYMyZero@users.noreply.github.com>
2021-11-30 17:17:52 +0800

    Update popup.js

--- copy/popup.js
+++ copy/popup.js
@@ -6,10 +6,8 @@
                     path:"img/on.png"
                 });
                 chrome.storage.local.get(["tabId"], function(result) {
-                    if(window.location.href.slice(0,9).indexOf("chrome://")!=-1){
-                        chrome.tabs.insertCSS(result.tabId, {file: 'css/copy.css'});
-                        chrome.tabs.executeScript(result.tabId, {code: 'try {document.getElementById("reader-container").setAttribute("id","")} catch (error) {console.log(error)}'});
-                    }
+                    chrome.tabs.insertCSS(result.tabId, {file: 'css/copy.css'});
+                    chrome.tabs.executeScript(result.tabId, {code: 'try {document.getElementById("reader-container").setAttribute("id","")} catch (error) {console.log(error)}'});
                 });
                 chrome.storage.local.set({"stu": "1"}, function() {});
             }else{
