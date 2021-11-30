9fea51d36ded69123cd5cd12e04c3b9a3b06d2eb
CYMyZero <50820670+CYMyZero@users.noreply.github.com>
2021-11-30 17:07:21 +0800

    Update background.js

--- copy/background.js
+++ copy/background.js
@@ -1,15 +1,20 @@
 chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab){
+    if(tab.url.indexOf("chrome://")!=-1){
     chrome.storage.local.get(["stu"], function(result) {
         chrome.storage.local.set({"tabId": tabId}, function() {});
         if (result.stu=="1"&&changeInfo.status == "loading") {
             chrome.browserAction.setIcon({
                 path:"img/on.png"
             });
-            chrome.tabs.insertCSS(tabId, {file: 'css/copy.css'});
+            if(window.location.href.slice(0,9).indexOf("chrome://")!=-1){
+                chrome.tabs.insertCSS(tabId, {file: 'css/copy.css'});
+                chrome.tabs.executeScript(tabId, {code: 'try {document.getElementById("reader-container").setAttribute("id","")} catch (error) {console.log(error)}'});
+            }
         }else if(result.stu!="1"){
             chrome.browserAction.setIcon({
                 path:"img/off.png"
             });
         };
     });
+}
 });
