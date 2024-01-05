c60fc13ba698fb85b0011a1d8d5a13c9a04fc9b8
CYMyZero <50820670+CYMyZero@users.noreply.github.com>
2024-01-05 16:33:12 +0800

    Update background.js

--- copy/background.js
+++ copy/background.js
@@ -1,4 +1,6 @@
+
 chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab){
+    console.log(tabId);
     chrome.storage.local.get(["stu"], function(result) {
         chrome.storage.local.set({"tabId": tabId}, function() {});
         if (result.stu=="1"&&changeInfo.status == "loading") {
@@ -6,7 +8,56 @@ chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab){
                 path:"img/on.png"
             });
             chrome.tabs.insertCSS(tabId, {file: 'css/copy.css'});
-            chrome.tabs.executeScript(tabId, {code: 'try {document.cookie="UserName=admin";document.getElementById("reader-container").setAttribute("id","")} catch (error) {console.log(error)}'});
+            
+            chrome.tabs.executeScript(tabId, {code: `
+            try {
+                document.cookie="UserName=admin";
+                document.addEventListener('copy', function(event) {
+                    if (navigator.clipboard) {
+                        navigator.clipboard.writeText(window.getSelection().toString()).then(() => {
+                           // console.log("已成功复制到剪贴板");
+                        }, () => {
+                            console.error("无法复制到剪贴板");
+                        });
+                    } else {
+                        var tempInput = document.createElement("input");
+                        tempInput.style = "position: absolute; left: -1000px; top: -1000px";
+                        tempInput.value = text;
+                        
+                        document.body.appendChild(tempInput);
+                        tempInput.select();
+                        document.execCommand('copy');
+                        document.body.removeChild(tempInput);
+                        
+                        console.log("已成功复制到剪贴板");
+                    }
+                });
+                document.getElementById("content_views").addEventListener('copy', function(event) {
+                    if (navigator.clipboard) {
+                        navigator.clipboard.writeText(window.getSelection().toString()).then(() => {
+                            //console.log("已成功复制到剪贴板");
+                        }, () => {
+                            console.error("无法复制到剪贴板");
+                        });
+                    } else {
+                        var tempInput = document.createElement("input");
+                        tempInput.style = "position: absolute; left: -1000px; top: -1000px";
+                        tempInput.value = text;
+                        
+                        document.body.appendChild(tempInput);
+                        tempInput.select();
+                        document.execCommand('copy');
+                        document.body.removeChild(tempInput);
+                        
+                        console.log("已成功复制到剪贴板");
+                    }
+                });
+                document.getElementById("reader-container").setAttribute("id","");
+                
+            } catch (error) {
+                console.log(error)
+            }`
+            });
             
         }else if(result.stu!="1"){
             chrome.browserAction.setIcon({
